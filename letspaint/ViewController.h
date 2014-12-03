3d27929d6a20f0091160960827368712c313c766
Erin McGee <emcgee@Erins-MacBook-Pro.local>
2014-12-03 13:51:59 -0500

    Initial Commit

---
+++ letspaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  letspaint
+//
+//  Created by Erin McGee on 12/3/14.
+//  Copyright (c) 2014 Erin McGee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ letspaint/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  letspaint
+//
+//  Created by Erin McGee on 12/3/14.
+//  Copyright (c) 2014 Erin McGee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
